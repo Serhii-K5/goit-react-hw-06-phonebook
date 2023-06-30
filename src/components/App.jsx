d45946900cc9fc29d55856275577da92d0fdecb1
Serhii <116863810+Serhii-K5@users.noreply.github.com>
2023-06-30 16:57:05 +0300

    Made changes for Redux

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,7 @@
-import React, { useState, useEffect } from 'react';
+// import React, { useState, useEffect } from 'react';
+import React, { useEffect } from 'react';
 import PropTypes from 'prop-types';
-import { nanoid } from 'nanoid';
+// import { nanoid } from 'nanoid';
 import { ContactForm } from './ContactForm/ContactForm';
 import { Filter } from './Filter/Filter';
 import { ContactList } from './ContactList/ContactList';
@@ -8,26 +9,43 @@ import { PhonebookImg } from './PhonebookImg/PhonebookImg';
 import css from './styles/styles.module.css';
 import defaultData from './data/data.json'
 
+import { getContacts, getFilter } from 'redux/selectors';
+import { useSelector, useDispatch } from 'react-redux';
+import { addContact, delContact } from 'redux/actions';
+
 
 
 const KEY_LOCALSTORAGE = 'contactList';
 
 export const App = () => {
-  const [contacts, setContacts] = useState(() => {
+  // const [contacts, setContacts] = useState(() => {
+  //   const contactsFromLocalStorage = localStorage.getItem(KEY_LOCALSTORAGE);
+  //   return contactsFromLocalStorage === null ? defaultData : JSON.parse(contactsFromLocalStorage);    
+  // });
+  const contacts = useSelector(getContacts);
+  
+  // const [filter, setFilter] = useState('');
+  const filter = useSelector(getFilter);
+
+  const dispatch = useDispatch();
+
+  
+  // const [isFirstRender, setFlag] = useState(true);
+  // if (condition) {
+    
+  // }
+  
+  useEffect(() => {
     const contactsFromLocalStorage = localStorage.getItem(KEY_LOCALSTORAGE);
     return contactsFromLocalStorage === null ? defaultData : JSON.parse(contactsFromLocalStorage);    
-  });
-
-  const [filter, setFilter] = useState('');
+  }, []);
 
   useEffect(() => {
-    localStorage.setItem(KEY_LOCALSTORAGE, JSON.stringify(contacts));
-    
+    localStorage.setItem(KEY_LOCALSTORAGE, JSON.stringify(contacts));    
   }, [contacts]);
 
-
   const handleSubmit = evt => {
-    const id = nanoid();
+    // const id = nanoid();
     const name = evt.name;
     const number = evt.number;
     const contactsLists = [...contacts];
@@ -35,17 +53,18 @@ export const App = () => {
     if (contactsLists.findIndex(contact => name === contact.name) !== -1) {
       return alert(`${name} is already in contacts.`);
     } else {
-      contactsLists.push({id, name, number});      
+      // contactsLists.push({id, name, number}); 
+      dispatch(addContact(name, number));
     }
 
-    setContacts(contactsLists);
+    // setContacts(contactsLists);
     localStorage.setItem(KEY_LOCALSTORAGE, JSON.stringify(contactsLists));
   };
 
-  const handleChange = evt => {
-    const {value} = evt.target;
-    setFilter(value);
-  };
+  // const handleChange = evt => {
+  //   const {value} = evt.target;
+  //   setFilter(value);
+  // };
 
   const сontactFiltered = () => {
     const filterContactsList = contacts.filter(contact => {
@@ -58,22 +77,24 @@ export const App = () => {
   };
 
   const handleDelete = evt => {
-    setContacts(contacts.filter(contact => contact.id !== evt));
-
+    // setContacts(contacts.filter(contact => contact.id !== evt));
+    dispatch(delContact(evt));
   };
 
   return (      
     <div className={css.container}>
-      <h1 hidden>HW #4 (phonebook)</h1>        
+      {/* <h1 hidden>HW #4 (phonebook)</h1>         */}
       <div className={css.containerPhonebookStyles}>
         <PhonebookImg />
         <section> 
-          <h2>Phonebook</h2>
+          {/* <h2>Phonebook</h2> */}
+          <h1>Phonebook</h1>
           <ContactForm handleSubmit={handleSubmit} />          
         </section>
         <section>
           <h2> Contacts</h2>
-          <Filter filter={filter} handleChange={handleChange} />
+          {/* <Filter filter={filter} handleChange={handleChange} /> */}
+          <Filter />
           <ContactList
             contacts={сontactFiltered()}
             handleDelete={handleDelete}
--- src/components/Filter/Filter.jsx
+++ src/components/Filter/Filter.jsx
@@ -1,7 +1,13 @@
 import propTypes from 'prop-types';
 import css from './Filter.module.css';
 
-export const Filter = ({ filter, handleChange }) => (
+export const Filter = ({ filter, handleChange }) => {
+  const dispatch = useDispatch();
+  const filter = useSelector(getFilter);
+
+  const handleChange = e => {
+    dispatch(setFilter(e.target.value));
+  };
   <>
     <label className={css.filterLabel}>Find contacts by Name
       <input
@@ -14,7 +20,7 @@ export const Filter = ({ filter, handleChange }) => (
       />
     </label>
   </>
-);
+)};
 
 Filter.propTypes = {
   filter: propTypes.string.isRequired,
